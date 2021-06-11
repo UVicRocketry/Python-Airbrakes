8b6eec078d45efd74ebfa3e54382ea03dc5a10f0
Aila Simpson <14950228+TechGal@users.noreply.github.com>
2021-06-10 18:14:27 -0700

    Adding Pseudo Code to Github
    This is the pseudo code Kris write during a discussion on data storage and management

---
+++ pseudo.py
@@ -0,0 +1,62 @@
+#for airbrakes math
+import airBrakesMath as ab
+
+class RocketData:
+
+	__init__():
+
+	this.rocketdata = { 
+		this.bme = {
+			"humity": 0,
+			"temp": 0, 
+			"pressure": 0
+		},
+		this.imu = {
+			"yaw": 0,
+			"pitch": 0,
+			"roll": 0,
+			"linearacceration": 0,
+			"linearvelocity": 0
+		},
+		this.straingauges = {
+			"one": 0, 
+			"two": 0,
+			"three": 0,
+			"four": 0,
+			"five": 0, 
+			"six": 0,
+			"seven": 0,
+			"eight": 0,
+			"nine": 0,
+			"ten": 0,
+			"elven": 0,
+			"tweleve": 0,
+			"thirteen": 0  
+		},
+		this.encoders = {
+			"position": 0
+		}
+	}
+	def self.getRocketData(self.formated?):#parameter 0 or 1 for formatted data
+	
+	def self.setRocketData(self.rocketdata): # 
+	
+	def self.formate():
+	
+		
+	
+class threading:
+
+	#threading shit
+
+#child class of rocket data to send rocketdata??
+class SendData:
+	__init__():
+	
+	
+	def sendDataBlackBox():
+		#grabs data from Rocket data and send to black box as CSV
+	def sendDataAirBrakes():
+		#send updated from air brakes math to airbrakes
+		
+		
